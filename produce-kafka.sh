503ad1ca94a2b93f7aef3c931fe5e023f3ea215d
Gjore Milevski <dzole0311@gmail.com>
2025-06-15 18:55:27 +0200

    fix: replace deprecated --broker-list with --bootstrap-server in produce.sh

--- produce-kafka.sh
+++ produce-kafka.sh
@@ -4,5 +4,5 @@ TOPIC=${1:-test}
 
 echo "Producing to topic '$TOPIC' (Ctrl+D to stop)..."
 docker exec -i kafka kafka-console-producer.sh \
-  --broker-list localhost:9092 \
+  --bootstrap-server localhost:9092 \
   --topic "$TOPIC"
